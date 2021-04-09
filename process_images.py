1612e458e6b8753462f7ad022a33ef70cb38abff
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-04-09 11:48:37 -0400

    created resizing script

--- process_images.py
+++ process_images.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Process the cmd arguments to the script"
     )
-    parser.add_argument("-f", "--folder-id", type=int, default=0, help="The folder timestamp to use")
+    parser.add_argument("-f", "--folder-id", type=int, default=0, help="The folder timestamp to use; defaults to the last one")
 
     args = parser.parse_args()
     folder_id = args.folder_id
@@ -20,10 +20,15 @@ if __name__ == '__main__':
         folder_id = os.listdir("./data/imgs")[-1]
 
     img_files = os.listdir(f"./data/imgs/{folder_id}")
-    os.mkdir(f"./data/processed_images/{folder_id}")
+    
+    folder_path = f"./data/processed_images/{folder_id}"
+    if not os.path.exists(f"./data/processed_images/{folder_id}"):
+        os.mkdir(folder_path)
 
     print("Loaded file names into memory")
     print("Processing images . . .")
     for img_file in progressbar.progressbar(img_files):
-        img = process(cv2.imread(img_file))
+        img = cv2.imread(f"./data/imgs/{folder_id}/{img_file}")
+        img = process(img)
+        cv2.imwrite(f"{folder_path}/{img_file}", img)
     print("Finished")
