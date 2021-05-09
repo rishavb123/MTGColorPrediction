0aa7489056b2cf0ebf75dc2359ef77ae6a6ba4f1
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-05-09 11:51:51 -0400

    created model

--- model.py
+++ model.py
@@ -4,7 +4,38 @@ class ColorClassifier(tf.keras.Model):
 
     def __init__(self) -> None:
         super(ColorClassifier, self).__init__()
+        self.layers = [
+            tf.keras.layers.Conv2D(64, strides=(5, 5), input_shape=(156, 114, 3)), # (152, 110, 64)
+            tf.keras.layers.LeakyReLU(),
+            tf.keras.layers.MaxPooling2D((2, 2)), # (76,  55, 64)
+            tf.keras.layers.Dropout(0.3),
+
+            tf.keras.layers.Conv2D(128, strides=(3, 3)), # (74, 53, 128)
+            tf.keras.layers.MaxPooling2D((2, 2)), # (37, 26, 128)
+            tf.keras.layers.Dropout(0.3),
+
+            tf.keras.layers.Conv2D(128, strides=(2, 2)), # (36, 25, 128)
+            tf.keras.layers.MaxPooling2D((2, 2)), # (18, 12, 128)
+            tf.keras.layers.Dropout(0.3),
+
+            tf.keras.layers.Flatten(), # (18*12*128)
+
+            tf.keras.layers.Dense(2000, activation='relu'),
+            tf.keras.layers.Dropout(0.3),
+
+            tf.keras.layers.Dense(1000, activation='relu'),
+            tf.keras.layers.Dropout(0.3),
+            
+            tf.keras.layers.Dense(500, activation='relu'),
+            tf.keras.layers.Dropout(0.3),
+            
+            tf.keras.layers.Dense(5, activation='sigmoid')
+        ]
         
     def call(self, inputs, training=False):
-        return None
+        x = inputs
+        for layer in self.layers:
+            if training or not isinstance(layer, tf.keras.layers.Dropout):
+                x = layer(x)
+        return x
 
