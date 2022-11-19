d8f13af64d9dd392b30560e803dba525ba0ee0b3
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2022-11-18 21:52:33 -0500

    added tensorboard support

--- train.py
+++ train.py
@@ -7,6 +7,10 @@ from load_data import load_data
 from model import ColorClassifier
 from utils import time_func
 
+s = str(int(time.time() * 1000))
+log_dir = f"logs/tb/{s}"
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
 model = ColorClassifier()
 
 X, y = time_func(load_data, "Loading data into memory . . .")
@@ -17,8 +21,7 @@ optimizer = tf.keras.optimizers.Adam(LEARNING_RATE)
 cross_entropy = tf.keras.losses.BinaryCrossentropy()
 
 model.compile(optimizer=optimizer, loss=cross_entropy, metrics=['accuracy'])
-model.fit(X, y, epochs=10)
+model.fit(X, y, epochs=10, callbacks=[tensorboard_callback])
 
-s = str(int(time.time() * 1000))
 os.mkdir(f'./models/{s}')
 model.save(f'models/{s}/model')
